562e255750eeb78137deacf0794fe71d304329eb
Pavan datta Reddy <pvn.datta@gmail.com>
2024-12-13 09:56:46 -0500

    added insert query to gpu and cpu tables

--- server/sqlscript.py
+++ server/sqlscript.py
@@ -12,14 +12,47 @@ def import_csv_to_databse(csv_file):
         with open(csv_file, mode='r') as file:
             csv_reader = csv.reader(file)
             header = next(csv_reader)
-            sqlite_insert_query = """INSERT INTO Computer_Part(part_name, brand, size, date_posted, unit_price, slug) VALUES (?, ?, ?, ?, ?, ?)"""
+            sqlite_insert_query = """INSERT INTO Computer_Part(part_name, brand, size, date_posted, unit_price, slug, ComponentType, OtherInfo) VALUES (?, ?, ?, ?, ?, ?, ?, ?)"""
+
+
+            cpu_insert_query ="""INSERT INTO Cpu(part_id, cores) VALUES (?, ?)"""
+            gpu_insert_query = """INSERT INTO Gpu(part_id, vram) VALUES (?, ?)"""
+            motherboard_insert_query = """INSERT INTO Motherboard(part_id, form_factor) VALUES (?, ?)"""
+            cooling_insert_query = """INSERT INTO Cooling(part_id, method) VALUES (?, ?)"""
+            memory_insert_query =  """INSERT INTO Storage_Device(part_id, memory) VALUES (?, ?)"""
+            computer_case_insert_query = """INSERT INTO Computer_case(part_id, size) VALUES (?, ?)"""
+
+
 
             for row in csv_reader: 
                 cursor.execute(sqlite_insert_query, row)
 
+                part_id = cursor.lastrowid
 
-        sqliteConnection.commit()
-        print("Record of {cursor.rowcount} inserted successfully into SqliteDb_developers table", cursor.rowcount); 
+                component_type = str(row[6])
+                
+                if component_type.__eq__('GPU'):
+                    vram = row[7]
+                    cursor.execute(gpu_insert_query, (part_id, vram))
+                    sqliteConnection.commit()
+                
+                if component_type.__eq__('CPU'):
+                    cores = row[7]
+
+                    try: 
+                        cores = int(cores)
+                        cursor.execute(cpu_insert_query, (part_id, cores))
+                        sqliteConnection.commit()
+                    except error:
+                        print(f"Warning: Invalid cores value for CPU with part_id {part_id}: {cores}")
+                        continue  # Skip this row if the cores value is invalid
+                        
+
+
+                sqliteConnection.commit()
+                    
+        
+        print(f"Record of {cursor.rowcount} inserted successfully into SqliteDb_developers table", cursor.rowcount); 
         cursor.close()
 
     except sqlite3.Error as error:
