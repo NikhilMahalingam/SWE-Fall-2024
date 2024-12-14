9fd4d8519066aaab89f1f0ffbd388d0a5e09d4dd
ak2113 <ak2113@scarletmail.rutgers.edu>
2024-12-13 22:50:12 -0500

    Finished Display of Home Page

--- client/src/routes/Home.js
+++ client/src/routes/Home.js
@@ -1,6 +1,12 @@
 import React from 'react'
 import pcCooling from '../assets/images/PCCooling3.jpg';
+import ManWithComputer from '../assets/images/ManWIthComputer.png';
 import '../assets/css/Home.css';
+import Laptop from '../assets/images/laptop.png';
+import GeForce from '../assets/images/GeForce.png';
+import Ssd from '../assets/images/ssd.png';
+import Headphones from '../assets/images/Headphones.png';
+import Controller from '../assets/images/Controller.png';
 
 function Home() {
   return (
@@ -14,8 +20,51 @@ function Home() {
     >
       <header className="Home-header">
         <h1>Welcome to PC Composer!</h1>
-        <p>Compose beautiful personal computers using our chatbot,</p>
-        <p>prebuilts, or custom-made database of parts.</p>
+        <p>Want to compose beautiful personal computers?</p>
+        <p>computers? PC Composer easily achieves this</p>
+        <p>using our chatbot, prebuilts, and custom-made</p>
+        <p>database of parts.</p>
+
+        <img
+          src={ManWithComputer}
+          alt ="Man with a Computer"
+          className = "Home-page-image"
+        />
+        <h3>Our Parts</h3>
+        <h5>We have an abundance of parts for users to checkout</h5>
+
+      
+        <div className = "Home-images-container">
+          <img
+            src={Laptop}
+            alt ="Laptop"
+            className = "Home-page-Laptop"
+          />
+
+          <img
+            src={GeForce}
+            alt ="GeForce"
+            className = "Home-page-GeForce"
+          />
+
+          <img
+            src={Ssd}
+            alt ="Ssd"
+            className = "Home-page-Ssd"
+          />
+
+          <img
+            src={Headphones}
+            alt ="Headphones"
+            className = "Home-page-Headphones"
+          />
+
+          <img
+            src={Controller}
+            alt ="Controller"
+            className = "Home-page-Controller"
+          />
+        </div>
       </header>
     </div>
   );
