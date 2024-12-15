db0184c82b1eb7ab98cc952314f07999089fb50b
jeremywhui <jeremywhui@gmail.com>
2024-12-15 16:08:08 -0500

    Add Pre-builts to Cart

--- client/src/routes/AppRoutes.js
+++ client/src/routes/AppRoutes.js
@@ -20,7 +20,7 @@ const AppRoutes = ({cart, user, onCartChange, onUserChange}) => {
       <Route path="/" element={<Home />} />
       <Route path="/cart" element={<Cart cart={cart} onCartChange={onCartChange}  user={user} onUserChange={onUserChange} />} />
       <Route path="/parts" element={<Parts cart={cart} onCartChange={onCartChange} user={user} onUserChange={onUserChange} />}/>
-      <Route path="/prebuilts" element={<Prebuilts cart={cart} onCartChange={onCartChange}/>} />
+      <Route path="/prebuilts" element={<Prebuilts cart={cart} onCartChange={onCartChange} user={user} onUserChange={onUserChange}/>} />
       <Route path="/chatbot" element={<Chatbot user={user} onUserChange={onUserChange}/>} />
     </Routes>
   );
